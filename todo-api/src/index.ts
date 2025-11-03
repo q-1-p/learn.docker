f60f8ae8ab8e2e9a1f6f11588d6403f298800043
q-1-p <github.pubilc@outlook.com>
2025-11-03 10:05:35 +0900

    チュートリアル完了

---
+++ todo-api/src/index.ts
@@ -0,0 +1,60 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors"; // 追加
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+const app = new Hono();
+
+// 追加
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/todos", (c) => {
+  return c.json({ todos });
+});
+
+app.post("/todos", async (c) => {
+  const { title } = await c.req.json();
+  const todo: Todo = {
+    id: todos.length + 1,
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return c.json({ todo });
+});
+
+app.put("/todos/:id", async (c) => {
+  const { id } = c.req.param();
+  const { completed } = await c.req.json();
+  const todo = todos.find((todo) => todo.id === Number(id));
+  if (!todo) {
+    return c.notFound();
+  }
+  todo.completed = completed;
+  return c.json({ todo });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
